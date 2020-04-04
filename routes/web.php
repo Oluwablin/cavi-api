d0d9cbb596284e106efc71152c4e749ba1a760e5
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2020-04-04 09:15:00 +0100

    fixed route merge conflict

--- routes/web.php
+++ routes/web.php
@@ -22,19 +22,11 @@ Route::resource('project', 'ProjectController');
 
 // TODO: modify the route to match what ever you want on your frontend.
 Route::post('/add/project', 			'ProjectController@addProject');
-<<<<<<< HEAD
-Route::get('/fetch/all/added/project', 	'ProjectController@allProjects');
-Route::get('/this/can/be/anything', 	'ProjectController@oneProjectById');
-Route::put('/this/can/be/anything', 	'ProjectController@updateProject');
-Route::delete('/this/can/be/anything', 	'ProjectController@deleteProject');
-Route::delete('/this/can/be/anything', 	'ProjectController@deleteAllProject');
-=======
-Route::get('/fetch/all/projects', 	'ProjectController@allProjects');
-Route::get('/fetch/one/project/by/id/{id}', 	'ProjectController@oneProjectById');
-Route::put('/update/project', 	'ProjectController@updateProject');
-Route::delete('/delete/project', 	'ProjectController@deleteProject');
+Route::get('/fetch/all/projects', 		'ProjectController@allProjects');
+Route::get('/fetch/one/project/by/id/{id}', 'ProjectController@oneProjectById');
+Route::put('/update/project', 			'ProjectController@updateProject');
+Route::delete('/delete/project', 		'ProjectController@deleteProject');
 Route::delete('/delete/all/projects', 	'ProjectController@deleteAllProject');
->>>>>>> dev
 Route::get('/fetch/project/list', 		'ProjectController@projects');
 Route::get('/fetch/stack/list', 		'ProjectController@stacks');
 Route::get('/fetch/proficiency/list', 	'ProjectController@proficiency');
